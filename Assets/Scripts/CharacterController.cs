5eef6acf1e17cfcca7b3ed472c83c8ae340cbb7b
JGuo-UIC <56518284+JGuo-UIC@users.noreply.github.com>
2020-01-28 18:23:44 -0600

    Add files via upload

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed = 15.0f;
+    public float rotationSpeed = 5;
+    public float gravity = 700f;
+
+    private Rigidbody rb;
+    private Transform t;
+    private float centerBody = 0;
+    
+    //public float mouseMovement = 100f; //Sensitivity of a Mouse
+    //public Transform character; //Player Character
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        t = GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Forward and Backward movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            //rb.velocity += (this.transform.forward * speed * Time.deltaTime);
+            rb.velocity += Time.deltaTime * speed * this.transform.forward;
+        }else if (Input.GetKey(KeyCode.S))
+        {
+            //rb.velocity -= this.transform.forward * speed * Time.deltaTime;
+            rb.velocity -= Time.deltaTime * speed * this.transform.forward;
+        }
+        
+        //Jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(t.up * gravity);
+        }
+
+        //Mouse Movement for player rotation
+        if (Input.GetAxis("Mouse X") < 0)
+        {
+            //t.Rotate((Vector3.up) * +rotationSpeed);
+            t.rotation = Quaternion.Euler(0, rotationSpeed*Time.deltaTime, 0);
+        }
+        if (Input.GetAxis("Mouse Y") < 0)
+        {
+            //t.Rotate((Vector3.down) * -rotationSpeed);
+            t.rotation = Quaternion.Euler(0, -rotationSpeed*Time.deltaTime, 0);
+        }
+        //float rotateX = Input.GetAxis("Mouse X") * mouseMovement * Time.deltaTime; //Mouse movement X-axis
+        //float rotateY = Input.GetAxis("Mouse y") * mouseMovement * Time.deltaTime; //Mouse movement Y-axis
+
+        //character.Rotate(Vector3.up * xmm);
+    }
+}
---
+++ Assets/Scripts/CharacterController2.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2 : MonoBehaviour
+{
+    public float speed = 8.0f;
+    public float rotationSpeed = 90;
+    public float force = 1.5f;
+    public float gravity = 200.0f;
+    public GameObject cannon;
+    public GameObject bullet;
+
+    private Rigidbody rb;
+    private Transform t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        t = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Forward/Backward
+        if (Input.GetKey(KeyCode.W))
+            rb.velocity += Time.deltaTime * speed * this.transform.forward;
+        else if (Input.GetKey(KeyCode.S))
+            rb.velocity -= Time.deltaTime * speed * this.transform.forward;
+        
+        //Left/Right
+        float direction = Input.GetAxis("Horizontal");
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = transform.right * force;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = -transform.right * force;
+        }
+        
+        //Rotation
+        if (Input.GetKey(KeyCode.E))
+            t.rotation *= Quaternion.Euler(0, rotationSpeed * Time.deltaTime, 0);
+        else if (Input.GetKey(KeyCode.Q))
+            t.rotation *= Quaternion.Euler(0, - rotationSpeed * Time.deltaTime, 0);
+        
+        //Jump
+        if (Input.GetKeyDown(KeyCode.Space))
+            rb.AddForce(t.up * gravity);
+        
+        //Firing
+        if (Input.GetMouseButtonDown(0)){
+
+            GameObject newBullet = GameObject.Instantiate(bullet, cannon.transform.position, cannon.transform.rotation) as GameObject;
+            newBullet.GetComponent<Rigidbody>().velocity += Vector3.up * 5;
+            newBullet.GetComponent<Rigidbody>().AddForce(newBullet.transform.forward * 500);
+            Destroy(newBullet, 5f);
+        }
+    }
+}
